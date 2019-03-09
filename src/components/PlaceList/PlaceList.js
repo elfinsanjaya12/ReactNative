07e9e57a7d509b88eaac93c4f9c92c5e11f05a17
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-03-09 21:04:30 +0700

    Scrollview

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 
 import ListItem from "../ListItem/ListItem";
 
@@ -11,7 +11,7 @@ const placeList = props => {
       onItemPressed={() => props.onItemDeleted(i)}
     />
   ));
-  return <View style={styles.listContainer}>{placesOutput}</View>;
+  return <ScrollView style={styles.listContainer}>{placesOutput}</ScrollView>;
 };
 
 const styles = StyleSheet.create({
